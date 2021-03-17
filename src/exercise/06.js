0b1f7e45c252a05b7508bd62e58233c4b32d319a
reginaldoteixeira <regis.tx@gmail.com>
2021-03-17 00:49:06 +0000

    (16/03) Finalização do módulo

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -3,7 +3,7 @@
 
 import * as React from 'react'
 
-function UsernameForm({onSubmitUsername}) {
+function UsernameForm({ onSubmitUsername }) {
   // 🐨 add a submit event handler here (`handleSubmit`).
   // 💰 Make sure to accept the `event` as an argument and call
   // `event.preventDefault()` to prevent the default behavior of form submit
@@ -19,37 +19,71 @@ function UsernameForm({onSubmitUsername}) {
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
 
+  // Criando uma ref, um recurso do React para acessar o conteúdo
+  // de elementos de formulários
   const usernameEl = React.useRef()
 
+  // Criar uma variável de estado do React
+  // Uma variável de estado mantém uma informação mesmo que o conteúdo da página
+  // seja atualizado
+  // Para ler o conteúdo da variável de estado, podemos acessá-la diretamente.
+  // No entanto, para alterar seu conteúdo, usamos uma função set.
+
+  // error -> variável de estado
+  // setError -> função de atualização da variável de estado
+  // A função useState aceita um parâmetro que é o VALOR INICIAL da variável
+  // de estado. Ou seja, nesse caso, error tem um valor inicial de string vazia.
+
+  // useState() retorna um vetor que normalmente é recebido via desestruturação
+  const [msg, setMsg] = React.useState('')
+  const [username, setUsername] = React.useState('')
+
   function handleSubmit(event) {
-    event.preventDefault() // Previne o recarregamento do formulário
+    event.preventDefault()  // Previne o recarregamento do formulário
     //const username = document.getElementById('username').value
     //const username = document.querySelector('#username').value
     const username = usernameEl.current.value
     onSubmitUsername(username)
- }  
+  }
+
+  function handleChange(event) {
+    // Capturando o valor do input
+    const val = event.target.value
+
+    // Armazena na variável de estado o valor do imput já convertido
+    // para minúsculas
+    setUsername(val.toLowerCase())
+
+    /*
+    // O input será válido se seu conteúdo for idêntico
+    // ao próprio conteúdo em minúsculas
+    const isValid = (val === val.toLowerCase())
+
+    // Atualizado o estado
+    setMsg(isValid ? '' : 'O valor informado deve estar em minúsculas.')
+    */
+
+  }
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label>Username:</label>
-        {/* Associando o ref usernameEl ao imput */}
-        <input ref={usernameEl} id="username" type="text" />
+        {/* Associando o ref usernameEl ao input */}
+        <input ref={usernameEl} id="username" type="text" onChange={handleChange} value=
+        {username} />
       </div>
+      {/* O conteúdo da variável de estado pode ser lido sem necessidade de função auxiliar */}
+      <div style={{ color: 'red'}}>{msg}</div>
       <button type="submit">Submit</button>
     </form>
   )
+
 }
-
-
- 
-
 
 function App() {
   const onSubmitUsername = username => alert(`You entered: ${username}`)
   return <UsernameForm onSubmitUsername={onSubmitUsername} />
 }
-
-
 
 export default App
